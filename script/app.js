ff902962b782de645f36dae6b37589f1b1fde9c6
mkropidlowski <mkropidlowsky@gmail.com>
2020-10-20 22:21:26 +0200

    update color message

--- script/app.js
+++ script/app.js
@@ -1,26 +1,31 @@
 
-// const usernameEmail = document.querySelector('.usernameEmail');
-
 const inputMessage = document.querySelector('#inputMessage');
 
 
+const randomColor = () => {
+    const color = Math.floor(Math.random()*16777215).toString(16);
+    
+    localStorage.setItem('userColor', color);
+
+}
+
+randomColor();
 
 
 class Chat {
-    constructor(username, chatColor){
-        this.chatColor = chatColor;
+    constructor(username){
         this.username = username;
         this.chats = db.collection('post');
     }
 
-    async addChat(message, chatColor){
+    async addChat(message){
 
-     
+        const getColor = localStorage.getItem('userColor');
         const date = new Date();
 
         const chatMessage = {
             message,
-            chatColor,
+            color: getColor,
             username: this.username,    
             created_at: firebase.firestore.Timestamp.fromDate(date)
         };
@@ -43,45 +48,29 @@ class Chat {
     }
 
 
-    getUsername(username,chatColor){
+    getUsername(username){
         this.username = username;
-        this.chatColor = chatColor;
-
-        localStorage.setItem('username', username);
-        localStorage.setItem('color', chatColor);
-       
+    
+        localStorage.setItem('username', username);    
     }
-    
 
+   
+        
+
+    
 }
-
-
-    
 
 
 inputMessage.addEventListener('submit', e => {
     e.preventDefault();
 
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  
-    chat.addChat(inputMessage['addUserMessage'].value, randomColor)
+
+    chat.addChat(inputMessage['addUserMessage'].value)
         .then(() => console.log('chat added!'))
         .catch(err => console.log(err));
 
     inputMessage.reset();
 });
-
-
-
-// const circle = document.querySelector('.circle');
-
-// randomUserColor() {
-
-//     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-//     const random = circle.style.backgroundColor = "#" + randomColor;
-
-     
-// }
 
 
 
--- script/ui.js
+++ script/ui.js
@@ -6,17 +6,10 @@ class ChatUI {
 
     render(data){
 
-        // const randomColorProfile = document.querySelectorAll('.randomColorProfile');
-        // const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        // //     const random = circle.style.backgroundColor = "#" + randomColor;
-        // console.log(localStorage.getItem('color'));
-
-        // style="background-color:#${localStorage.getItem('color')};"
-
 
         const html = `
             <div class="userMessageBox">
-            <div class="userProfile randomColorProfile" style="background-color:#${data.chatColor}">${((data.username).charAt(0)).toUpperCase()}</div><div class="msgCloud">${data.message}</div>
+            <div class="userProfile randomColorProfile" style="background-color:#${data.color}">${((data.username).charAt(0)).toUpperCase()}</div><div class="msgCloud">${data.message}</div>
             </div>
             `;
 
